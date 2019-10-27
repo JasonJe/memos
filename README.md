3ac2b40e98ce6a8aea12162d93291210d5f7e0a4
jasonje <jasonje@foxmail.com>
2019-10-27 23:47:57 +0800

    拉勾爬取并可视化

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # :memo: `Memos!`
 
+## 语言学习相关
+
+* [`Go by Example` 练习](./go_example)
+
 ## 爬虫相关
 
 * [离线瓦片地图](./offline_tileserver)
@@ -15,6 +19,10 @@
 * [`Flask`实现`api token`](./redis_token)
 
 * [基于`Potree`的`3D`点云模型展示](./cloud_point)
+
+## 数据分析相关
+
+* 拉勾爬取并可视化
 
 ## 机器学习相关
 
---
+++ jobs_data_analysis/crawl.py
@@ -0,0 +1,220 @@
+import sys 
+import requests
+import random
+import time
+import json
+import pandas as pd 
+
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+
+key_word   = 'python后端'
+work_year  = '1-3年,3年及以下,3-5年,5-10年,10年以上'
+city       = '深圳'
+page_range = 31
+
+def get_user_agent():
+    user_agent_list = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Mozilla/5.0 (iPad; CPU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1",
+        "Mozilla/5.0 (Windows NT 6.1; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.110 Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36 OPR/46.0.2597.32",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 OPR/45.0.2552.898",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36 OPR/46.0.2597.39",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
+    ]
+    return random.choice(user_agent_list)
+
+def get_proxys():
+    urls = []
+    for i in range(1, page_range):
+        # urls.append('http://www.xicidaili.com/nn/%s'%i)
+        urls.append('https://www.kuaidaili.com/free/inha/%s/'%i)
+    proxys  = []
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36'
+    }
+    for url in urls:
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.text, 'html5lib')
+        ips = soup.find_all('tr')
+        for i in range(1, len(ips)):
+            ip = ips[i]
+            tds = ip.find_all('td')
+            print(tds[0].contents[0], tds[1].contents[0])
+            proxys.append('http://' + tds[0].contents[0] + ':' + tds[1].contents[0])
+        time.sleep(3)
+    return proxys 
+
+def get_proxy(proxys):
+    return random.choice(proxys)
+
+def get_job_details(page_url, proxys, url_start):
+    headers = {
+        'Host'                     : 'www.lagou.com',
+        'Upgrade-Insecure-Requests': '1',
+        'Accept'                   : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Referer'                  : quote('https://www.lagou.com/jobs/list_{}/p-city_215?&cl=false&fromSearch=true&labelWords=&suginput='.format(key_word)),
+        'User-Agent'               : get_user_agent()
+    }
+
+    proxy_addr   = get_proxy(proxys)
+    proxies      = {proxy_addr.split(":")[0]: proxy_addr}
+
+    s        = requests.Session()
+    s.get(url_start, headers = headers, proxies = proxies, timeout = 3)
+    cookies  = s.cookies
+
+    page_resp    = requests.get(page_url, headers = headers, proxies = proxies, timeout = 10, cookies = cookies)
+    soup = BeautifulSoup(page_resp.text, "html5lib")
+
+    job_details  = soup.find(class_="job_bt").text.replace("\n", "").strip()
+    work_addr    = soup.find(class_="work_addr").text.replace("\n", "").replace(" ", "").replace("查看地图", "").strip()
+    position_lng = soup.find('input', {'name' : "positionLng"}).get('value', 0)
+    position_lat = soup.find('input', {'name' : "positionLat"}).get('value', 0)
+    active_time  = soup.find_all('span',  {'class': "tip"})[-1].text.replace(" ", "").replace("\n", "")
+    print(headers, proxies, cookies)
+    return job_details, work_addr, position_lng, position_lat, active_time
+
+def main():
+    proxys    = get_proxys()
+    url_start = "https://www.lagou.com/jobs/list_{}/p-city_215?px=default&gj={}".format(key_word, work_year)
+    url_parse = "https://www.lagou.com/jobs/positionAjax.json?city={}&needAddtionalResult=false".format(city)
+    
+    result = []
+    for x in range(1, page_range):
+        data = {
+            'first': 'true',
+            'pn': str(x),
+            'kd': key_word
+        }
+        headers = {
+            'Host'                     : 'www.lagou.com',
+            'Upgrade-Insecure-Requests': '1',
+            'Accept'                   : 'application/json, text/javascript, */*; q=0.01',
+            'Referer'                  : quote('https://www.lagou.com/jobs/list_{}/p-city_215?&cl=false&fromSearch=true&labelWords=&suginput='.format(key_word)),
+            'User-Agent'               : get_user_agent()
+        }
+        proxy_addr = get_proxy(proxys)
+        proxies = {proxy_addr.split(":")[0]: proxy_addr}
+
+        s = requests.Session()
+        s.get(url_start, headers = headers, proxies = proxies, timeout = 3)  # 请求首页获取cookies
+        cookie   = s.cookies  # 为此次获取的cookies
+        response = s.post(url_parse, data = data, headers = headers, proxies = proxies, cookies = cookie, timeout = 3)  # 获取此次文本
+        time.sleep(2)
+        response.encoding = response.apparent_encoding
+        text = json.loads(response.text)
+        info = text["content"]["positionResult"]["result"]
+        for i in info:
+            page_url    = 'https://www.lagou.com/jobs/{}.html'.format(i["positionId"]) 
+            print('Get ... {}'.format(page_url))
+              
+            try:
+                job_detail, work_addr, position_lng, position_lat, active_time = get_job_details(page_url, proxys, url_start)
+            except Exception as e:
+                print(e)
+                try:
+                    time.sleep(3)
+                    job_detail, work_addr, position_lng, position_lat, active_time = get_job_details(page_url, proxys, url_start)
+                except Exception as e:
+                    print('Retry second failed ...')
+                    print(e)
+                    job_detail, work_addr, position_lng, position_lat, active_time = '', '', 0.0, 0.0, ''
+            
+            result.append({
+                'companyFullName': i["companyFullName"],
+                'positionName'   : i["positionName"],
+                'companySize'    : i["companySize"],
+                'salary'         : i["salary"],
+                'skillLables'    : i["skillLables"],
+                'createTime'     : i["createTime"],
+                'district'       : i["district"],
+                'stationname'    : i["stationname"],
+                'workYear'       : i["workYear"],
+                'pageUrl'        : page_url,
+                'jobDetail'      : job_detail,
+                'workAddr'       : work_addr,
+                'positionLng'    : position_lng,
+                'positionLat'    : position_lat,
+                'activeTime'     : active_time
+            })
+            time.sleep(3)
+    df = pd.DataFrame(result)
+
+    df.to_csv('./data/python_backend.csv')
+
+if __name__ == '__main__':
+    main()
